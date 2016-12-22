aafbdc8da80e0abe7a02ce8b2f7b3330d0135244
tholapz <tholapz@gmail.com>
2016-12-21 22:27:50 -0800

    deploy

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <p>I'm a software engineer from Thailand. I'm currently working at a startup company called Vixlet. I create a revolutionary web application and proprietary widget system.</p>
           <p>I graduated from University of Southern California in 2013 with the Master's degree in Computer Science.</p>
           <p>In my spare time, I attend hackathon events in either LA or Bay Area. Apart from that, I read about Startup and History. My current book is Cracking the Coding Interview by Gayle Laakmann McDowell</p>
-          <p>Check out <a href="http://tholapz.com/reading.md">my reading list here</a>.</p>
+          <p>Check out <a href="https://github.com/tholapz/resume/blob/gh-pages/reading.md">my reading list here</a>.</p>
         </div>
         <p><a href="http://tholapz.com/resume.pdf">
           <span className="glyphicon glyphicon-download"></span> Download a copy of my resume
