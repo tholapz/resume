39fccb2636ef5cdad96becd60ea864c9798e9799
tholapz <tholapz@gmail.com>
2016-12-21 21:14:23 -0800

    add download link

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './bootstrap.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <a href="http://tholapz.com/resume.pdf">
+          <span className="glyphicon glyphicon-download"></span> Download a PDF copy
+        </a>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
